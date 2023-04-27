7aca84e598dc238c01a0b8b8f46e85b200d9ee11
Aceticia <xl3942nyu.edu>
2023-04-27 17:04:24 -0400

    workers

--- scripts/train_models.sh
+++ scripts/train_models.sh
@@ -1,5 +1,5 @@
 python src/train.py -m seed=0,1,2,3,4 hydra/launcher=submitit_slurm \
-  model/net=resnet18,resnet34 model.net.subspace_size=64,128 \
+  model/net=resnet18,resnet34 model.net.subspace_size=64,128 trainer=gpu data.num_workers=8 \
   hydra.launcher.partition=oermannlab hydra.launcher.gres="gpu:a100:1" +hydra.launcher.additional_parameters.time="3-00:00:00" \
   hydra.launcher.cpus_per_task=8 hydra.launcher.mem_per_cpu=10G \
  +hydra.launcher.additional_parameters.qos=qos_free
