35c112a32732d67b7f9d1f7c7f5078044578275c
Aceticia <xl3942nyu.edu>
2023-04-27 14:52:47 -0400

    add global multiply modulator

--- src/modulators/basic.py
+++ src/modulators/basic.py
@@ -1,6 +1,8 @@
 import torch
 from dataclasses import dataclass
 
+# TODO: Write generalized normalized modulator for the different multiply modulators
+
 @dataclass
 class Average:
     alpha: float = 0.5
@@ -9,6 +11,7 @@ class Average:
     
     def __str__(self):
         return f"Average(alpha={self.alpha})"
+
 
 @dataclass
 class MultiplyChannelWise:
@@ -51,6 +54,34 @@ class MultiplySpaceWise:
 
         # Find the max over each feature
         y_max = y.max(dim=1, keepdim=True)[0]
+
+        # Normalize y
+        y = y / y_max
+
+        # Map y to [alpha, beta]
+        y = self.alpha + (self.beta - self.alpha) * y
+
+        # Multiply
+        return x * y
+
+
+@dataclass
+class MultiplyGlobal:
+    alpha: float = 0.75
+    beta: float = 1.25
+
+    def __str__(self) -> str:
+        return f"MultiplySpaceWise(alpha={self.alpha},beta={self.beta})"
+
+    def __call__(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        # Find magnitude of y
+        y = torch.abs(y)
+
+        # Find the max over each feature
+        y_max = y.flatten(1).max(dim=1, keepdim=True)[0]
+
+        # Reshape to broadcast
+        y_max = y_max.view(-1, 1, 1, 1)
 
         # Normalize y
         y = y / y_max
