a0bcf8fef2a82d84d9fd27d3cb8248ca66d4f3f5
Aceticia <xl3942nyu.edu>
2023-05-03 15:29:57 -0400

    pairs script

--- scripts/run_pairs.sh
+++ scripts/run_pairs.sh
@@ -1,13 +1,25 @@
 # Run the resnet18 models
-python src/eval_all_pairs.py -m store_path=outs/resnet18_0 model/net=resnet18 ckpt_path=logs/train/multiruns/2023-04-24_19-03-20/0/checkpoints/epoch_020.ckpt
-python src/eval_all_pairs.py -m store_path=outs/resnet18_1 model/net=resnet18 ckpt_path=logs/train/multiruns/2023-04-24_19-03-20/1/checkpoints/epoch_035.ckpt
-python src/eval_all_pairs.py -m store_path=outs/resnet18_2 model/net=resnet18 ckpt_path=logs/train/multiruns/2023-04-24_19-03-20/2/checkpoints/epoch_040.ckpt
-python src/eval_all_pairs.py -m store_path=outs/resnet18_3 model/net=resnet18 ckpt_path=logs/train/multiruns/2023-04-24_19-03-20/3/checkpoints/epoch_020.ckpt
-python src/eval_all_pairs.py -m store_path=outs/resnet18_4 model/net=resnet18 ckpt_path=logs/train/multiruns/2023-04-24_19-03-20/4/checkpoints/epoch_022.ckpt
+python src/eval_all_pairs.py -m store_path=outs/resnet18_64_0 model/net=resnet18 ckpt_path=checkpoints/src.models.components.resnet.ResNet18Model_64_0.ckpt
+python src/eval_all_pairs.py -m store_path=outs/resnet18_64_1 model/net=resnet18 ckpt_path=checkpoints/src.models.components.resnet.ResNet18Model_64_1.ckpt
+python src/eval_all_pairs.py -m store_path=outs/resnet18_64_2 model/net=resnet18 ckpt_path=checkpoints/src.models.components.resnet.ResNet18Model_64_2.ckpt
+python src/eval_all_pairs.py -m store_path=outs/resnet18_64_3 model/net=resnet18 ckpt_path=checkpoints/src.models.components.resnet.ResNet18Model_64_3.ckpt
+python src/eval_all_pairs.py -m store_path=outs/resnet18_64_4 model/net=resnet18 ckpt_path=checkpoints/src.models.components.resnet.ResNet18Model_64_4.ckpt
+
+python src/eval_all_pairs.py -m store_path=outs/resnet18_128_0 model.net.subspace_size=128 model/net=resnet18 ckpt_path=checkpoints/src.models.components.resnet.ResNet18Model_128_0.ckpt
+python src/eval_all_pairs.py -m store_path=outs/resnet18_128_1 model.net.subspace_size=128 model/net=resnet18 ckpt_path=checkpoints/src.models.components.resnet.ResNet18Model_128_1.ckpt
+python src/eval_all_pairs.py -m store_path=outs/resnet18_128_2 model.net.subspace_size=128 model/net=resnet18 ckpt_path=checkpoints/src.models.components.resnet.ResNet18Model_128_2.ckpt
+python src/eval_all_pairs.py -m store_path=outs/resnet18_128_3 model.net.subspace_size=128 model/net=resnet18 ckpt_path=checkpoints/src.models.components.resnet.ResNet18Model_128_3.ckpt
+python src/eval_all_pairs.py -m store_path=outs/resnet18_128_4 model.net.subspace_size=128 model/net=resnet18 ckpt_path=checkpoints/src.models.components.resnet.ResNet18Model_128_4.ckpt
 
 # Then resnet34
-python src/eval_all_pairs.py -m store_path=outs/resnet34_0 model/net=resnet34 ckpt_path=logs/train/multiruns/2023-04-24_19-03-20/0/checkpoints/epoch_041.ckpt
-python src/eval_all_pairs.py -m store_path=outs/resnet34_1 model/net=resnet34 ckpt_path=logs/train/multiruns/2023-04-24_19-03-20/1/checkpoints/epoch_032.ckpt
-python src/eval_all_pairs.py -m store_path=outs/resnet34_2 model/net=resnet34 ckpt_path=logs/train/multiruns/2023-04-24_19-03-20/2/checkpoints/epoch_033.ckpt
-python src/eval_all_pairs.py -m store_path=outs/resnet34_3 model/net=resnet34 ckpt_path=logs/train/multiruns/2023-04-24_19-03-20/3/checkpoints/epoch_046.ckpt
-python src/eval_all_pairs.py -m store_path=outs/resnet34_4 model/net=resnet34 ckpt_path=logs/train/multiruns/2023-04-24_19-03-20/4/checkpoints/epoch_048.ckpt
+python src/eval_all_pairs.py -m store_path=outs/resnet34_64_0 model/net=resnet34 ckpt_path=checkpoints/src.models.components.resnet.ResNet34Model_64_0.ckpt
+python src/eval_all_pairs.py -m store_path=outs/resnet34_64_1 model/net=resnet34 ckpt_path=checkpoints/src.models.components.resnet.ResNet34Model_64_1.ckpt
+python src/eval_all_pairs.py -m store_path=outs/resnet34_64_2 model/net=resnet34 ckpt_path=checkpoints/src.models.components.resnet.ResNet34Model_64_2.ckpt
+python src/eval_all_pairs.py -m store_path=outs/resnet34_64_3 model/net=resnet34 ckpt_path=checkpoints/src.models.components.resnet.ResNet34Model_64_3.ckpt
+python src/eval_all_pairs.py -m store_path=outs/resnet34_64_4 model/net=resnet34 ckpt_path=checkpoints/src.models.components.resnet.ResNet34Model_64_4.ckpt
+
+python src/eval_all_pairs.py -m store_path=outs/resnet34_128_0 model.net.subspace_size=128 model/net=resnet34 ckpt_path=checkpoints/src.models.components.resnet.ResNet34Model_128_0.ckpt
+python src/eval_all_pairs.py -m store_path=outs/resnet34_128_1 model.net.subspace_size=128 model/net=resnet34 ckpt_path=checkpoints/src.models.components.resnet.ResNet34Model_128_1.ckpt
+python src/eval_all_pairs.py -m store_path=outs/resnet34_128_2 model.net.subspace_size=128 model/net=resnet34 ckpt_path=checkpoints/src.models.components.resnet.ResNet34Model_128_2.ckpt
+python src/eval_all_pairs.py -m store_path=outs/resnet34_128_3 model.net.subspace_size=128 model/net=resnet34 ckpt_path=checkpoints/src.models.components.resnet.ResNet34Model_128_3.ckpt
+python src/eval_all_pairs.py -m store_path=outs/resnet34_128_4 model.net.subspace_size=128 model/net=resnet34 ckpt_path=checkpoints/src.models.components.resnet.ResNet34Model_128_4.ckpt
